a8ae5c1e231d36e7f6fb181a6b537e46b6def8cf
MoPaMo <mpm1301@web.de>
2021-02-20 09:25:57 +0100

    test swappable

--- public/create.js
+++ public/create.js
@@ -1,3 +1,7 @@
-const sortable = new Sortable.default(document.querySelectorAll('ul'), {
-    draggable: 'li'
+const sortable = new Sortable.default(document.querySelectorAll('#sort-app'), {
+    draggable: 'div'
   });
+  sortable.on('sortable:start', () => console.log('sortable:start'));
+sortable.on('sortable:sort', () => console.log('sortable:sort'));
+sortable.on('sortable:sorted', () => console.log('sortable:sorted'));
+sortable.on('sortable:stop', () => console.log('sortable:stop'));
